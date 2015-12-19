4eda2650d6e40809069d99d804768dfe97ce8767
William Ashton <William@AshtonFam.org>
2015-12-19 12:10:49 -0500

    Modify history bar styles

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -1,9 +1,10 @@
 function HTMLActuator() {
-    this.tileContainer = document.querySelector(".tile-container");
-    this.scoreContainer = document.querySelector(".score-container");
-    this.bestContainer = document.querySelector(".best-container");
-    this.messageContainer = document.querySelector(".game-message");
+    this.tileContainer         = document.querySelector(".tile-container");
+    this.scoreContainer        = document.querySelector(".score-container");
+    this.bestContainer         = document.querySelector(".best-container");
+    this.messageContainer      = document.querySelector(".game-message");
     this.savedMessageContainer = document.querySelector(".saved-game-message");
+    this.historyContainer      = document.querySelector("#history");
 
     this.score = 0;
 }
